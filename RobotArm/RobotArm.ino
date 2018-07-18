13b22c61dcb57fb615ef51c3b05b1d44b7945ebe
Benjamin Lawrence <benjaminlawrence@Benjamins-MacBook-Pro.local>
2018-07-18 17:35:41 -0400

    defined all necessary functions thus far

--- RobotArm/RobotArm.ino
+++ RobotArm/RobotArm.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <math.h>
 
 // pin number for the button that starts the ultrasonic scan
 int button = 3;
@@ -13,14 +14,19 @@ const int echoPin = 10;
 long duration;
 int distance;
 
-// point object in yz plane to store boundary points
+// point object in xyz system to store boundary points
 struct Point {
+  int x
   int y;
   int z;
 }
-// Boundaries of object in yz plane
+// Boundaries of object in xyz system
 // Up to 10 boundary points. More complex objects will be approximated with 10 points
 Point[] coordinates = new Point [10];
+
+// if arm is not used within a certain amount of time, then perform a random movement
+long randNumber;
+long timer;
 
 // "Depending on your servo make, the pulse width min and max may vary, you 
 // want these to be as small/large as possible without hitting the hard stop
@@ -49,6 +55,15 @@ void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
+
+  // random number between 1 and 4 hours
+  randNumber = random(1, 4);
+  timer = millis() + randNumber*3.6*pow(10, 6);
 }
 
 int getDistance() {
@@ -86,9 +101,27 @@ void scanArea() {
   
 }
 
-// algorithm used to determine the location of the object to be picked up
 void findObject() {
+  // algorithm used to determine the location of the object to be picked up
   
+}
+
+void calculateMovements() {
+  // calculates the necessary servo angles to move the arm so that the tip of the claw is at the x position of the object closest
+  // to the claw
+}
+
+void moveArmToInitialPosition() {
+  // move the arm to the actual position calculated in the calculateMovements method
+}
+
+void scanDepth() {
+  // use the IR LED and sensor to scan the depth of the object
+  
+}
+
+void grabObject() {
+  // (finally) grab the object and hold it in place
 }
 
 void loop() {
@@ -96,8 +129,23 @@ void loop() {
   if (digitalRead(button)) {
     scanArea();
     findObject();
+    calculateMovements();
+    moveArmToInitialPosition();
+    scanDepth();
+    grabObject();
+
+    // random number between 1 and 4 hours
+    randNumber = random(1, 4);
+    timer = millis() + randNumber*3.6*pow(10, 6);
   }
 
+  if (millis > timer) {
+    // perform random movement
+
+    // random number between 1 and 4 hours
+    randNumber = random(1, 4);
+    timer = millis() + randNumber*3.6*pow(10, 6);
+  }
 
   delay(10);
 }
